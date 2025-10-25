c13039813f021ee970c80c039226155637b99c83
johnwayne97xy <wfen0016@student.monash.edu>
2025-10-25 16:55:08 +1100

    setup github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 如果部署到 GitHub Pages，必须写仓库名
+  // 比如 https://johnwayne97xy.github.io/FIT5032LAB/
+  base: '/FIT5032LAB/',
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+
+  server: {
+    port: 5173,
+    open: true
   }
 })
